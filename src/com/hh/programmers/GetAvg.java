3fcdc71d2239c54dc539916a437b31ff7c032414
HyehyeonLee <july4th_@naver.com>
2021-03-31 00:23:33 +0900

    commit

--- src/com/hh/programmers/GetAvg.java
+++ src/com/hh/programmers/GetAvg.java
@@ -2,6 +2,7 @@ package com.hh.programmers;
 
 public class GetAvg {
     //평균 구하기
+
     //정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
     public double solution(int[] arr) {
         double answer = 0;
