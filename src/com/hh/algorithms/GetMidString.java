3e82936c99fa35736bc7ed8e901c4c1fc7bfe2e2
HyehyeonLee <july4th_@naver.com>
2021-03-31 00:01:24 +0900

    20210330

---
+++ src/com/hh/algorithms/GetMidString.java
@@ -0,0 +1,27 @@
+package com.hh.algorithms;
+
+import java.util.StringTokenizer;
+
+public class GetMidString {
+    //단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요.
+    // 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+	public GetMidString() {
+		super();
+	}
+	
+    public String solution(String s){
+        String answer = "";
+        if(s.length() % 2 == 1) {
+            answer = s.substring((s.length()/2), (s.length()/2)+1);
+        }else {
+            answer = s.substring((s.length()/2)-1, (s.length()/2)+1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    	String s = "qwer";
+    	GetMidString gm = new GetMidString();
+    	System.out.println(gm.solution(s));
+    }
+}
