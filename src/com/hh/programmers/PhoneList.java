8f1673a5e883e56966b4971cb0ea31f4e9c761b9
HyehyeonLee <july4th_@naver.com>
2021-04-03 15:18:32 +0900

    91.7 : time over and not hash

--- src/com/hh/programmers/PhoneList.java
+++ src/com/hh/programmers/PhoneList.java
@@ -5,34 +5,20 @@ import java.util.HashMap;
 
 public class PhoneList {
     //전화번호 목록
-    public PhoneList(){
+    public PhoneList() {
         super();
     }
 
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        HashMap<String, Integer> hash = new HashMap<>();
-        int idx = 0;
-        for(String phone : phone_book){
-            hash.put(phone, idx);
-
-        }
-
-        for(String phone : phone_book){
-            for(String key : hash.keySet()){
-                if(phone.startsWith(key) && !phone.equals(key)){
-                    hash.put(phone, idx+1);
+        for (String phone : phone_book) {
+            for (String key : phone_book) {
+                if (phone.startsWith(key) && !phone.equals(key)) {
+                    answer = false;
+                    break;
                 }
             }
         }
-
-        if(hash.containsValue(1)){
-            answer = false;
-        }
-
-
-        //System.out.println(hash.toString());
-
         return answer;
     }
 
@@ -40,7 +26,5 @@ public class PhoneList {
         PhoneList phone = new PhoneList();
         String[] phone_book = {"119", "97674223", "1195524421"};
         System.out.println(phone.solution(phone_book));
-        String str = "1195524421";
-        //System.out.println(str.substring(0,3));
     }
 }
