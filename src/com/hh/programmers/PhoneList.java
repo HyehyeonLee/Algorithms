0c8bd633458152ad9aebe20cd0ea928ca79c0cf5
HyehyeonLee <july4th_@naver.com>
2021-04-03 12:23:29 +0900

    time over

--- src/com/hh/programmers/PhoneList.java
+++ src/com/hh/programmers/PhoneList.java
@@ -11,13 +11,35 @@ public class PhoneList {
 
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        for(int i = 0; i< phone_book.length; i++){
-            for(int j = 0; j<=i; j++){
-                if(phone_book[i].startsWith(phone_book[j]) && i != j){
-                    answer = false;
+        HashMap<String, Integer> hash = new HashMap<>();
+        int idx = 0;
+        for(int i = 0; i<phone_book.length; i++){
+                if(!hash.containsKey(phone_book[i])){
+                    hash.put(phone_book[i], idx);
+                }else{
+                    hash.put(phone_book[i], idx+1);
+                }
+        }
+
+
+        for(String phone : hash.keySet()){
+            for(int j = 0; j< hash.size(); j++){
+                if(phone.startsWith(phone_book[j]) && !phone.equals(phone_book[j])){
+                    hash.put(phone, idx+1);
                 }
             }
         }
+
+        int flag = 0;
+        for(int v : hash.values()){
+            flag += v;
+        }
+
+        if(flag != 0){
+            answer = false;
+        }
+
+        System.out.println(hash.toString());
 
         return answer;
     }
@@ -26,5 +48,7 @@ public class PhoneList {
         PhoneList phone = new PhoneList();
         String[] phone_book = {"119", "97674223", "1195524421"};
         System.out.println(phone.solution(phone_book));
+        String str = "1195524421";
+        //System.out.println(str.substring(0,3));
     }
 }
