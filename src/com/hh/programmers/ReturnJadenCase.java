827808363c32b104f56e981d96100709ce8d56b5
HyehyeonLee <july4th_@naver.com>
2021-04-07 21:22:41 +0900

    93.8

---
+++ src/com/hh/programmers/ReturnJadenCase.java
@@ -0,0 +1,31 @@
+package com.hh.programmers;
+
+import java.util.Locale;
+
+public class ReturnJadenCase {
+    public ReturnJadenCase(){
+        super();
+    }
+
+    public String solution(String s) {
+        String answer = "";
+        char c[] = s.toLowerCase(Locale.ROOT).toCharArray();
+
+        for(int i = 0; i<c.length; i++){
+            if(c[i] == ' ' && (c[i+1] >= (char)97 && c[i+1] <= (char)122)){
+                c[i+1] = (char) (c[i+1] - 32);
+            }else if(i == 0 && (c[i] >= (char)97 && c[i] <= (char)122)){
+                c[i] = (char) (c[i] - 32);
+            }
+            answer += c[i];
+        }
+
+        //for(int)
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ReturnJadenCase sol = new ReturnJadenCase();
+        System.out.println(sol.solution("3people unFollowed me"));
+    }
+}
