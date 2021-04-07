2fea5a046523773a2376fb49562cf80f65ec5b4e
HyehyeonLee <july4th_@naver.com>
2021-04-07 22:11:07 +0900

    JadenCase

--- src/com/hh/programmers/ReturnJadenCase.java
+++ src/com/hh/programmers/ReturnJadenCase.java
@@ -12,20 +12,19 @@ public class ReturnJadenCase {
         char c[] = s.toLowerCase(Locale.ROOT).toCharArray();
 
         for(int i = 0; i<c.length; i++){
-            if(c[i] == ' ' && (c[i+1] >= (char)97 && c[i+1] <= (char)122)){
-                c[i+1] = (char) (c[i+1] - 32);
+            if(c[i] == ' ' && i != c.length-1 && (c[i+1] >= (char)97 && c[i+1] <= (char)122)){
+                c[i + 1] -= 32;
             }else if(i == 0 && (c[i] >= (char)97 && c[i] <= (char)122)){
-                c[i] = (char) (c[i] - 32);
+                c[i] -= 32;
             }
             answer += c[i];
         }
 
-        //for(int)
         return answer;
     }
 
     public static void main(String[] args) {
         ReturnJadenCase sol = new ReturnJadenCase();
-        System.out.println(sol.solution("3people unFollowed me"));
+        System.out.println(sol.solution("3people unFollowed me "));
     }
 }
