55c7d49181c1a853bc57f87a05beae3bc47a551a
HyehyeonLee <july4th_@naver.com>
2022-02-19 00:43:37 +0900

    lotto 80

--- src/com/hh/programmers/LottoMaxMin.java
+++ src/com/hh/programmers/LottoMaxMin.java
@@ -5,44 +5,46 @@ import java.util.Collections;
 
 public class LottoMaxMin {
 	// 로또의 최고 순위와 최저 순위
-	//정확성: 46.7
-	//합계: 46.7 / 100.0
+	// 정확성: 80.0
+	// 합계: 80.0 / 100.0
+	
 	public int[] solution(int[] lottos, int[] win_nums) {
 		int[] answer = new int[2];
-		ArrayList<Integer> lottoList = new ArrayList<Integer>();
-		ArrayList<Integer> winList = new ArrayList<Integer>();
-		for(int i = 0; i<lottos.length; i++) {
-			lottoList.add(lottos[i]);
-			winList.add(win_nums[i]);
-		}
-		
-		Collections.sort(lottoList);
-		Collections.sort(winList);
 		
 		int[] rank = { 6, 6, 5, 4, 3, 2, 1 };
 		int zeroCount = 0;
 		int minIdx = 0;
 		int maxIdx = 0;
 		
-		for (int i = 0; i < lottoList.size(); i++) {
-			if (lottoList.get(i) == 0) {
+		for (int i = 0; i < lottos.length; i++) {
+			if (lottos[i] == 0) {
 				zeroCount++;
 			}
 
-			for (int j = 0; j < winList.size(); j++) {
-				if (lottoList.get(i) == winList.get(j)) {
+			for (int j = 0; j < win_nums.length; j++) {
+				if (lottos[i] == win_nums[j]) {
 					minIdx++;
-					winList.remove(i);
+					win_nums[j] = 99;
 				}
 			}
 		}
 		
-		if(zeroCount > winList.size()) {
-			maxIdx = minIdx + winList.size();
-		}else if(zeroCount < winList.size()){
-			maxIdx = minIdx + zeroCount;
-		}else {
-			maxIdx = minIdx;
+		int remWinNum = 0;
+		for(int i = 0; i<win_nums.length; i++) {
+			if(win_nums[i] == 99) {
+				remWinNum++;
+			}
+		}
+		
+		remWinNum = win_nums.length - remWinNum;
+		maxIdx = minIdx;
+		
+		if(zeroCount > remWinNum) {
+			maxIdx += remWinNum;
+		}else if(zeroCount < remWinNum){
+			maxIdx += zeroCount;
+		}else if(zeroCount == 6) {
+			maxIdx = zeroCount;
 		}
 
 		answer[0] = rank[maxIdx];
@@ -53,8 +55,8 @@ public class LottoMaxMin {
 
 	public static void main(String[] args) {
 		LottoMaxMin sol = new LottoMaxMin();
-		int[] lottos = { 45, 4, 35, 20, 3, 9 };
-		int[] win_nums = { 20, 9, 3, 45, 4, 35 };
+		int[] lottos = { 0, 0, 0, 0, 0, 0 };
+		int[] win_nums = { 38, 19, 20, 40, 15, 25 };
 		int[] answer = sol.solution(lottos, win_nums);
 		for(int a : answer) {
 			System.out.print(a + " ");
